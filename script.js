26c3f10de8d32e8a27a8c0442755450e211f7078
dipanshuarya <155765771+dipanshuarya@users.noreply.github.com>
2024-01-20 20:10:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-const key = 12c553a1d208463ca6effa857adcfc94
+const apiKey = "12c553a1d208463ca6effa857adcfc94";
+const url = "https://newsapi.org/v2/everything?q=";
+const div = document.querySelector(".top-container")
 
-https://newsapi.org/v2/everything?q=tesla&from=2023-12-07&sortBy=publishedAt&apiKey=12c553a1d208463ca6effa857adcfc94
+window.addEventListener("load" , () => {
+    fetchNews("India")
+})
+
+const fetchNews = async (query) => {
+   const response = await fetch(`${url}${query}&apikey=${apiKey}`)
+   const data = await response.json()
+  
+   bindData(data.articles)
+}
+
+
+
+
+const bindData = (data) => {
+
+   data.forEach((items) => {
+    //  console.log(items);
+    if (!items.urlToImage) return;
+     div.innerHTML += `<div class="cards-container container flex ">
+    <div class="card">
+      <div class="card-header">
+      <a href="${items.url}">
+         <img src="${items.urlToImage}" alt="">
+         </a>
+      </div>
+      <div class="card-content">
+        <h4 id="news-title">${items.content}</h4>
+        <h6 class="news-source">${items.author}</h6>
+        <p class="new-desc" id="news-desc">${items.title}.</p>
+      </div>
+    </div>
+  </div>`;
+
+
+   })
+}
+
+
